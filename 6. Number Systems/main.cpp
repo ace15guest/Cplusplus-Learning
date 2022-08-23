df2bd5a6d4f99dd5b839a0a16e66acfd5af82648
Asa Guest <acg70@duke.edu>
2022-08-23 14:05:56 -0400

    Numbers

--- 6. Number Systems/main.cpp
+++ 6. Number Systems/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main(){
-    auto result = 10 <= 20;
-    std::cout << result << std::endl;
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3 = 0x0f; // Hexadecimal
+
+    std::cout << "number1: " << number1 << std::endl;
+    std::cout << "number2: " << number2 << std::endl;
+    std::cout << "number3: " << number3 << std::endl;
+    
     return 0;
 }

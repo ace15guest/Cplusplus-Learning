8c3e858b1df31da8353b89898124d9bdceed36d7
Asa Guest <acg70@duke.edu>
2022-08-23 15:52:08 -0400

    IntMods

--- 8. IntegerModifiers/main.cpp
+++ 8. IntegerModifiers/main.cpp
@@ -4,22 +4,18 @@
 using namespace std;
 
 int main(){
-    //Braced Initioalizers 
+    int value1 {10};
+    signed int value2 {10};
+    signed int value3 {-120};
     
-    int elephant_count; // Variable may contain garbage . WARNING
+    cout << sizeof(value1) << endl;
+    cout << sizeof(value2) << endl;
+    cout << sizeof(value3) << endl;
 
-    int lion_count{}; // init to 0 
 
-    int dog_count{10}; // init to 10 
 
-    int cat_count{15}; // init to 15
 
-    int domestic_animal {dog_count + cat_count};
-    
-    // This will throw an error    int narrowing_conversion {2.9}
-    // This wll not throw an error which is dangerous int narrowing_conversion (2.9)
-    cout << "Domestic Animals: " << domestic_animal << endl;
-    cout << sizeof(domestic_animal) << endl;
+
 
     return 0;
 }

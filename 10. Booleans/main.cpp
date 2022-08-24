041bd05c87cca0e8327f4b8249eb61d332f17e04
Asa Guest <acg70@duke.edu>
2022-08-24 09:51:14 -0400

    10

--- 10. Booleans/main.cpp
+++ 10. Booleans/main.cpp
@@ -18,6 +18,11 @@ int main(){
     else{
         cout << "STOP NOOOO" << endl;
     }
+    cout << green_light << endl; // 0
+
+    cout << boolalpha;
+
+    cout << green_light << endl; // False
 
     return 0;
 }

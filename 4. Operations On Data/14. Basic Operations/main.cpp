8a0770877053da4ccebef22d90a73952b8c577bd
Asa Guest <acg70@duke.edu>
2022-08-24 11:27:04 -0400

    Done

---
+++ 4. Operations On Data/14. Basic Operations/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // + - % / 
+    // Addition
+    int num1 = 2;
+    int num2 = 15;
+    int res1 = num1 + num2;
+    // Substraction
+    int res2 = num1-num2;
+    // Divide
+    int res3 =num2/num1;
+    // Multiplication
+    int res4=num1*num2;
+    // Modulus 
+    int res5= num2%num1;
+    cout << "res1: " << res1 << endl;
+    cout << "res2: " << res2 << endl;
+    cout << "res3: " << res3 << endl;
+    cout << "res4: " << res4 << endl;
+    cout << "res5: " << res5 << endl;
+
+    return 0;
+}

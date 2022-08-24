c609f99624c7918b22f3f67070fc9b0b5555d38d
Asa Guest <acg70@duke.edu>
2022-08-24 09:58:43 -0400

    11

---
+++ 11. CharactersAndText/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   char value = 'a'; // ASCII code for A
+   cout << "Value: " << value << endl;
+   cout << "Static Cast: " << static_cast<int>(value) << endl;
+
+    return 0;
+}

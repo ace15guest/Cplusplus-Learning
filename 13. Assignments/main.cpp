b9d3526aed253adfe0ef242b11d4de6d6a801fda
Asa Guest <acg70@duke.edu>
2022-08-24 10:43:01 -0400

    13

---
+++ 13. Assignments/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int var1{123};// Declare and Initialization
+    cout << "var1: " << var1 <<endl;
+
+    var1 =55; // Assign
+    cout << "var1: " << var1 <<endl;
+
+    return 0;
+}

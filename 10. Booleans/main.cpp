08ecd4e8810c963cfa598547e7e3588b641a2382
Asa Guest <acg70@duke.edu>
2022-08-24 09:46:09 -0400

    11

---
+++ 10. Booleans/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool red_light {true};
+    bool green_light {false};
+
+    if (red_light == true){
+        cout << "STOP!" << endl;
+    }
+    else{
+        cout << "GO!" << endl;
+    }
+
+    return 0;
+}

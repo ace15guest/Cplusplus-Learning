371e082f204b93be5d4f2c2b5c3100823e47f22d
Asa Guest <acg70@duke.edu>
2022-08-30 16:15:24 -0400

    Pointers

--- 8. Pointers/34. Declaring and Using Pointers/main.cpp
+++ 8. Pointers/34. Declaring and Using Pointers/main.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main(){
-    // Read beyond bounds
-    int numbers [] {1,2,3,4,5};
-    cout << numbers[10] << endl;
-    return 0;
+    //Initialize nullptr
+    int *p_number {}; // Can only store address of a variable of type int
+    double *p_fractional_number {};// Can only store address of a variable of type double
+    // Explicitly initialize nullptr (Not pointing anywhere)
+    int *p_number1{nullptr};
+    int *p_fractional_number1 {nullptr};
+
+    //All pointers will be the same size
+    cout << "Null Pointer: " << p_number << endl;
+    cout << "Size of int:" << sizeof(p_fractional_number1) << "\n" << "Size of double: " << sizeof(p_fractional_number) <<endl;
+
+    int int_var {43};
+    int *p_int{&int_var};
+    cout << "int_var value -- memory location: " << int_var << " -- " << p_int << endl;
+
+    //Dereferencing Pointers 
+    int* p_int2 {nullptr};
+    int int_data {56};
+    p_int2 = &int_data;
+    cout << "Value of pointer: " << *p_int2 << endl;
 }

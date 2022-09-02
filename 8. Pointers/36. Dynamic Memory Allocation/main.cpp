21934eb7cd3eceab9295438bf861c8312d2f49cb
Asa Guest <acg70@duke.edu>
2022-09-02 14:07:10 -0400

    p

--- 8. Pointers/36. Dynamic Memory Allocation/main.cpp
+++ 8. Pointers/36. Dynamic Memory Allocation/main.cpp
@@ -4,23 +4,35 @@ using namespace std;
 
 int main(){
 
- int* p_number4{nullptr};
+ int *p_number4{nullptr}; //pointing to nowhere
  p_number4 = new int; // Dynamically allocate space for a single int on the heap
                       // Memory in this location is ours until we give it back
+                      // The size of this memory location is enough to hold an int and only an int
 
 *p_number4 = 77;
 cout << "Dynamically allocating memory: " << endl;
-cout << "*p_number4: " << p_number4 << " What is the diff " << &p_number4 << " IDK " << *p_number4<< endl;
+cout << "p_number4: " << p_number4 << " \n&p_number4; " << &p_number4 << "\n*p_number4: " << *p_number4<< endl;
 
 int *p_number5 {new int};
 int *p_number6 {new int (22)};
 int *p_number7 {new int {23}};
 
  cout << "p_number5: " << p_number5 << endl;
- cout << "*p_number5: " << *p_number5 << endl;
+ cout << "*p_number5: " << *p_number5 << endl; // Junk Value
  
  cout << "p_number6: " << p_number6 << endl;
  cout << "*p_number6: " << *p_number6 << endl;
+
+ delete p_number4;
+ p_number4 = nullptr;
+
+ delete p_number5;
+ p_number5 = nullptr;
+
+ delete p_number6;
+ p_number6 = nullptr;
+
+
 
 
 

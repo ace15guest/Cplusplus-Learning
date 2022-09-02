66769084139e8a740efc8a8b3d11ea9f822f065c
Asa Guest <acg70@duke.edu>
2022-09-02 13:44:29 -0400

    Now

--- 8. Pointers/36. Dynamic Memory Allocation/main.cpp
+++ 8. Pointers/36. Dynamic Memory Allocation/main.cpp
@@ -3,8 +3,25 @@
 using namespace std;
 
 int main(){
-    // Read beyond bounds
-    int numbers [] {1,2,3,4,5};
-    cout << numbers[10] << endl;
-    return 0;
+
+ int* p_number4{nullptr};
+ p_number4 = new int; // Dynamically allocate space for a single int on the heap
+                      // Memory in this location is ours until we give it back
+
+*p_number4 = 77;
+cout << "Dynamically allocating memory: " << endl;
+cout << "*p_number4: " << p_number4 << " What is the diff " << &p_number4 << " IDK " << *p_number4<< endl;
+
+int *p_number5 {new int};
+int *p_number6 {new int (22)};
+int *p_number7 {new int {23}};
+
+ cout << "p_number5: " << p_number5 << endl;
+ cout << "*p_number5: " << *p_number5 << endl;
+ 
+ cout << "p_number6: " << p_number6 << endl;
+ cout << "*p_number6: " << *p_number6 << endl;
+
+
+
 }
